d08c58707a5f4eb426e32c4feb2f51fb977afe67
Jose Tellez <jdrgv999@gmail.com>
2022-04-25 09:36:35 -0500

    Hechas las funciones para calcular ls figuras geometricas

---
+++ js1-figuras-geometricas/codigo.js
@@ -0,0 +1,22 @@
+//const areaCuadrado = ladoCuadrado * ladoCuadrado;
+
+function perimetroCuadrado (lado) {
+    return lado * 4 + "cm"
+}
+
+function areaCuadrado (lado) {
+    return lado * lado + "cm2"
+}
+//Calculo del triangulo//
+
+console.log("A partir de ahora, los calculos del triangulo")
+
+function perimetroTriangulo (lado1, lado2, base) {
+    return lado1 + lado2 + base
+}
+
+function areaTriangulo (baseTriangulo, alturaTriangulo){
+    return (baseTriangulo * alturaTriangulo) / 2;
+}
+
+//Calculo del circulo//
